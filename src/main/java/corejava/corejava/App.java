94266eb004a51e8b5e56669fe06cac1bd56bec6c
kiransk <kirana.k@grofers.com>
2016-08-01 22:37:24 +0530

    first commit

--- src/main/java/corejava/corejava/App.java
+++ src/main/java/corejava/corejava/App.java
@@ -6,8 +6,48 @@ package corejava.corejava;
  */
 public class App 
 {
+	
+	
+	
+	public static int minimum(int[] a) 
+	{
+		int min = a[0];
+		
+		for (int i=0;i<=a.length;i++)
+		{
+			
+			if(min > a[i])
+				
+			{
+				
+			}
+			
+			
+			
+		}
+		
+		return min;
+		
+		
+	}
+	
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+     
+    	
+    	
+    	int[] a = {1,2 ,3,4};
+    	
+    	
+    	minimum(a);
+    	
+    	
+    	
+    	
+        
+        
+        
     }
 }
